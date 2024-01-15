ea03e766eb405eff435fcb9a96b3541a6cfe5f1e
Andrew-Dyachenko <north.inhale@gmail.com>
2024-01-16 03:55:47 +0400

    feat: page alpha + themes
    the page alpha is ready. Dark and Light themes are also added

--- index.js
+++ index.js
@@ -1,43 +1,24 @@
-document.addEventListener('DOMContentLoaded', () => {
-	const canvas = document.getElementById('dbd-logo');
-	canvas.width = 400;
-	canvas.height = 300;
-	const ctx = canvas.getContext('2d');
+document.addEventListener("DOMContentLoaded", () => {
+	const themeSwitcher = document.getElementById("theme-switcher");
+	const themeStylesheet = document.getElementById("theme-stylesheet");
 
-	const totalLines = 5;
-	const lineWidth = 100;
-	const tailLength = 20; // длина хвостиков
-	const animationDuration = 1000;
+	themeSwitcher.addEventListener("change", () => {
+		if (themeSwitcher.checked) {
+			document.body.classList.add("dark-theme");
+			themeStylesheet.setAttribute("href", "dark-theme.css");
+		} else {
+			document.body.classList.remove("dark-theme");
+			themeStylesheet.setAttribute("href", "light-theme.css");
+		}
+	});
 
-	function drawLine(index, color, startY, endY) {
-		ctx.beginPath();
-		ctx.moveTo(50 + index * lineWidth, startY);
-		ctx.lineTo(50 + index * lineWidth, endY);
-		ctx.strokeStyle = color;
-		ctx.lineWidth = 10;
-		ctx.lineJoin = 'round';
-		ctx.lineCap = 'round';
-		ctx.stroke();
-		ctx.closePath();
+	// Проверяем сохраненную тему в localStorage
+	const savedTheme = localStorage.getItem("theme");
+	if (savedTheme) {
+		themeSwitcher.checked = savedTheme === "dark";
+		if (savedTheme === "dark") {
+			document.body.classList.add("dark-theme");
+			themeStylesheet.setAttribute("href", "dark-theme.css");
+		}
 	}
-
-	function drawDiagonalLine(color, startX, startY, endX, endY, tailLength) {
-		ctx.beginPath();
-		ctx.moveTo(startX - tailLength, startY);
-		ctx.lineTo(endX + tailLength, endY);
-		ctx.strokeStyle = color;
-		ctx.lineWidth = 10;
-		ctx.lineJoin = 'round';
-		ctx.lineCap = 'round';
-		ctx.stroke();
-		ctx.closePath();
-	}
-
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-	for (let i = 0; i < totalLines - 1; i++) {
-		drawLine(i, '#A9A9A9', 50, 250);
-	}
-
-	drawDiagonalLine('#A9A9A9', 50, 50, 350, 250, tailLength);
 });
