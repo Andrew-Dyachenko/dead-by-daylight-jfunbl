fc219e8726fdd058569fe3f61dc62533910996ea
Andrew-Dyachenko <north.inhale@gmail.com>
2024-01-19 08:08:50 +0400

    feat: reduce motion + shadows fix
    An event handler has been added that is responsible for processing and tracking user settings of reduce motion, which will disable and enable animation if the user so desires. Aimed at accessibility as some users may not be comfortable with animation.
    Shadows previously was cropped, now it fixed.

---
+++ index.61dd0154.js
@@ -0,0 +1,180 @@
+// modules are defined as an array
+// [ module function, map of requires ]
+//
+// map of requires is short require name -> numeric require
+//
+// anything defined in a previous bundle is accessed via the
+// orig method which is the require for previous bundles
+
+(function (modules, entry, mainEntry, parcelRequireName, globalName) {
+  /* eslint-disable no-undef */
+  var globalObject =
+    typeof globalThis !== 'undefined'
+      ? globalThis
+      : typeof self !== 'undefined'
+      ? self
+      : typeof window !== 'undefined'
+      ? window
+      : typeof global !== 'undefined'
+      ? global
+      : {};
+  /* eslint-enable no-undef */
+
+  // Save the require from previous bundle to this closure if any
+  var previousRequire =
+    typeof globalObject[parcelRequireName] === 'function' &&
+    globalObject[parcelRequireName];
+
+  var cache = previousRequire.cache || {};
+  // Do not use `require` to prevent Webpack from trying to bundle this call
+  var nodeRequire =
+    typeof module !== 'undefined' &&
+    typeof module.require === 'function' &&
+    module.require.bind(module);
+
+  function newRequire(name, jumped) {
+    if (!cache[name]) {
+      if (!modules[name]) {
+        // if we cannot find the module within our internal map or
+        // cache jump to the current global require ie. the last bundle
+        // that was added to the page.
+        var currentRequire =
+          typeof globalObject[parcelRequireName] === 'function' &&
+          globalObject[parcelRequireName];
+        if (!jumped && currentRequire) {
+          return currentRequire(name, true);
+        }
+
+        // If there are other bundles on this page the require from the
+        // previous one is saved to 'previousRequire'. Repeat this as
+        // many times as there are bundles until the module is found or
+        // we exhaust the require chain.
+        if (previousRequire) {
+          return previousRequire(name, true);
+        }
+
+        // Try the node require function if it exists.
+        if (nodeRequire && typeof name === 'string') {
+          return nodeRequire(name);
+        }
+
+        var err = new Error("Cannot find module '" + name + "'");
+        err.code = 'MODULE_NOT_FOUND';
+        throw err;
+      }
+
+      localRequire.resolve = resolve;
+      localRequire.cache = {};
+
+      var module = (cache[name] = new newRequire.Module(name));
+
+      modules[name][0].call(
+        module.exports,
+        localRequire,
+        module,
+        module.exports,
+        this
+      );
+    }
+
+    return cache[name].exports;
+
+    function localRequire(x) {
+      var res = localRequire.resolve(x);
+      return res === false ? {} : newRequire(res);
+    }
+
+    function resolve(x) {
+      var id = modules[name][1][x];
+      return id != null ? id : x;
+    }
+  }
+
+  function Module(moduleName) {
+    this.id = moduleName;
+    this.bundle = newRequire;
+    this.exports = {};
+  }
+
+  newRequire.isParcelRequire = true;
+  newRequire.Module = Module;
+  newRequire.modules = modules;
+  newRequire.cache = cache;
+  newRequire.parent = previousRequire;
+  newRequire.register = function (id, exports) {
+    modules[id] = [
+      function (require, module) {
+        module.exports = exports;
+      },
+      {},
+    ];
+  };
+
+  Object.defineProperty(newRequire, 'root', {
+    get: function () {
+      return globalObject[parcelRequireName];
+    },
+  });
+
+  globalObject[parcelRequireName] = newRequire;
+
+  for (var i = 0; i < entry.length; i++) {
+    newRequire(entry[i]);
+  }
+
+  if (mainEntry) {
+    // Expose entry point to Node, AMD or browser globals
+    // Based on https://github.com/ForbesLindesay/umd/blob/master/template.js
+    var mainExports = newRequire(mainEntry);
+
+    // CommonJS
+    if (typeof exports === 'object' && typeof module !== 'undefined') {
+      module.exports = mainExports;
+
+      // RequireJS
+    } else if (typeof define === 'function' && define.amd) {
+      define(function () {
+        return mainExports;
+      });
+
+      // <script>
+    } else if (globalName) {
+      this[globalName] = mainExports;
+    }
+  }
+})({"bB7Pu":[function(require,module,exports) {
+// Function to check user preferences regarding animation
+const checkReducedMotionPreference = ()=>{
+    // Get all SVG elements using the class
+    const svgElements = document.querySelectorAll(".app__logo");
+    // Iterate through each SVG element
+    for (const svgElement of svgElements){
+        // Get all <animate> and <set> elements inside the current SVG
+        const animations = svgElement.querySelectorAll("animate, set");
+        // Get all <template> elements inside the current SVG
+        const templates = svgElement.querySelectorAll("template");
+        // Check if the user prefers reduced motion
+        const prefersReducedMotion = window.matchMedia("(prefers-reduced-motion: reduce)").matches;
+        // If the user prefers reduced motion
+        if (prefersReducedMotion) // Wrap each <animate> and <set> element in a <template>
+        for (const animation of animations){
+            const templateWrapper = document.createElement("template");
+            const clone = animation.cloneNode(true);
+            animation.replaceWith(templateWrapper);
+            templateWrapper.content.appendChild(clone);
+        }
+        else // If the user does not prefer reduced motion, unwrap <template> elements
+        for (const template of templates){
+            const clone = template.content.firstChild.cloneNode(true);
+            template.replaceWith(clone);
+        }
+    }
+};
+// Event listener for DOMContentLoaded
+document.addEventListener("DOMContentLoaded", checkReducedMotionPreference);
+// Add an event listener for changes in user preferences
+window.matchMedia("(prefers-reduced-motion: reduce)").addEventListener("change", checkReducedMotionPreference);
+
+},{}]},["bB7Pu"], "bB7Pu", "parcelRequire94c2")
+
+//# sourceMappingURL=index.61dd0154.js.map
